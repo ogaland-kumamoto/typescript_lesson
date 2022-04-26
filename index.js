6825c030f904b4473422948239e40bf8e768d323
ogaland-kumamoto <kumamoto.kouki@ogaland.kumamoto>
2022-04-27 00:45:23 +0900

    commit

--- index.js
+++ index.js
@@ -5,9 +5,30 @@ var negative = -0.12;
 var single = 'hello';
 var double = "hello";
 var back = "hello";
+// オブジェクトに型をつける方法
 var person = {
     name: 'John',
     age: 30
+};
+// 配列に方を付けるAarry型はこう使う
+var fruits = ['apple', 'orange', 'banana'];
+var fruit = fruits[0];
+// fruits.push(21);
+// Tuple型を使用して、決まった内容の配列を作る方法
+var book = ['business', 1500, false];
+book.push('hello');
+// Enumを使って、特定のまとまったグループのみを受け入れる列挙型を使う方法
+// 列挙された特定のまとまったグループのみを受け入れる
+var CoffeeSize;
+(function (CoffeeSize) {
+    CoffeeSize["SHORT"] = "SHORT";
+    CoffeeSize["TALL"] = "TALL";
+    CoffeeSize["GRANDE"] = "GRANDE";
+    CoffeeSize["VENTI"] = "VENTI";
+})(CoffeeSize || (CoffeeSize = {}));
+var coffee = {
+    hot: true,
+    size: CoffeeSize.TALL
 };
 console.log(person.name);
 // // let hello: string = 'hello';
--- index.ts
+++ index.ts
@@ -15,6 +15,29 @@ const person:  {
     age: 30
 }
 
+// 配列に方を付けるAarry型はこう使う
+const fruits: string[] = ['apple', 'orange', 'banana'];
+const fruit = fruits[0];
+// fruits.push(21);
+
+// Tuple型を使用して、決まった内容の配列を作る方法
+const book: [string,number, boolean] = ['business', 1500, false]
+book.push('hello');
+
+// Enumを使って、特定のまとまったグループのみを受け入れる列挙型を使う方法
+// 列挙された特定のまとまったグループのみを受け入れる
+enum CoffeeSize {
+    SHORT= 'SHORT',
+    TALL= 'TALL',
+    GRANDE= 'GRANDE',
+    VENTI= 'VENTI'
+}
+
+const coffee = {
+    hot:true,
+    size:CoffeeSize.TALL
+}
+
 console.log(person.name);
 
 // // let hello: string = 'hello';
