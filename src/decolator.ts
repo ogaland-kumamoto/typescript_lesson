d5a0f4f4286c8a74c7c12c51bcb22df200a4d2d4
ogaland-kumamoto <kumamoto.kouki@ogaland.com>
2022-06-26 20:56:10 +0900

    セクション9まで終了

--- src/decolator.ts
+++ src/decolator.ts
@@ -10,29 +10,89 @@ function Logging(message: string) {
         console.log(constuructor);
     }
 }
-
+console.dir(Logging)
 // 106. こうしてデコレータを使って簡易版のフレームワークを作成する
 function Component(template:string, selector:string) {
     console.log('Components Factory');
-    return function (constructor: { new(...args:any[]): {name:string} }) {
-        const mountedElement = document.querySelector(selector);
-        const instance = new constructor();
-        if(mountedElement){
-            mountedElement.innerHTML = template;
-            mountedElement.querySelector('h1')!.textContent = constructor.name;
+    // return function (constructor: { new(...args:any[]): {name:string} ) {
+    return function <T extends { new(...args:any[]): {name:string}}>(constructor: T)  {
+        // 107. 戻り値にクラスを指定して、新しいクラスを作り出す方法
+        return class extends constructor{
+            constructor(...args:any[]){
+                super(...args);
+                console.log('Component');
+                const mountedElement = document.querySelector(selector);
+                const instance = new constructor();
+                if(mountedElement){
+                    mountedElement.innerHTML = template;
+                    mountedElement.querySelector('h1')!.textContent = constructor.name;
+                }
+            }
         }
     }
 }
+// 109. 「プロパティーデコレータを使う方法」と「prototypeについて」
+//JavaScriptはFunctionもオブジェクト 
+function PropertyLogging(target:any, propertyKey:string) {
+    console.log('propertyLogging')
+    console.log('target')
+    console.log('propertyKey')
+}
+
+function MethodLogging(target:any, propertyKey:string, descriptor: PropertyDescriptor) {
+    console.log('methodLogging')
+    console.log('target')
+    console.log('propertyKey')
+    console.log('description')
+}
+// 112. 戻り値を使って、実践的なメソッドデコレータを使う方法
+function enumerable(isEnumerable: boolean) {
+    return function(target:any, propertyKey:string, descriptor: PropertyDescriptor){
+        return {
+            enumerable: isEnumerable
+        }
+    }
+}
+// 111. アクセサーデコレータはこう使う
+function AccessorLogging(target:any, propertyKey:string, descriptor: PropertyDescriptor) {
+    console.log('AccessorLogging')
+    console.log('target')
+    console.log('propertyKey')
+    console.log('description')
+}
+// 113. パラメータデコレータはこう使う
+function ParameterLogging(target:any, propertyKey:string, parameterIndex: number) {
+    console.log('ParameterLogging')
+    console.log('target')
+    console.log('propertyKey')
+    console.log('parameterIndex')
+}
+// @Logging
+// デコレーターは下から上に実行される。
+
 
 // @Logging
+// デコレーターは下から上に実行される。
 @Logging('Logging user')
 @Component('<h1>{{name}}</h1>' ,'#app')
 class User {
     name = 'Quill';
-    constructor(){
+    constructor(public _age:number){
         console.log('User created');
     }
+    @AccessorLogging
+    get age(){
+        return this._age
+    }
+    set age(value){
+        this._age = value;
+    }
+    @enumerable(false)
+    @MethodLogging
+    greeting(@ParameterLogging message: string){
+        console.log(message)
+    }
 }
-const user1 = new User();
-const user2 = new User();
-const user3 = new User();
+const user1 = new User(32);
+const user2 = new User(32);
+const user3 = new User(32);
